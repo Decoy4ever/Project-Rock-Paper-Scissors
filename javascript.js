30172ad5c394440012dbdaa6bf5440e55b2a5cbd
David <Dzilla398@gmail.com>
2023-02-18 12:00:35 +1030

    feature: Expanded a 1st round scenario where the computer always wins.
    Appended 3 extra if-else statements to show the possible scenarios a computer wins each Round and prints the console msg the computer wins. Added a scenario where both the computer and player played the exact same choice and printout msg It is a Tie

--- javascript.js
+++ javascript.js
@@ -10,11 +10,10 @@ function getComputerChoice()
     let index = Math.floor(Math.random()*choice.length);
 
     // create a var called randomGame that stores the computer choice at the random index position
-    let randomGame = choice[index];
+    return choice[index];
 
     // print out choice of the computer
-    // console.log(randomGame);
-    console.log(randomGame);
+    //console.log(randomGame);
 }
 
 
@@ -23,11 +22,12 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection)
 {
-    // Use a if statement to show possible conditions
+
+    // Use an if statement to show possible conditions if the player won
     // if (Rock > Scissors) print winner
     if(playerSelection === "Rock" && computerSelection === "Scissors")
     {
-        console.log("Player Wins! Paper beats Rock")
+        console.log("Player Wins! Rock beats Scissors")
     }
     // if(Scissors > Paper ) print winner
     else if(playerSelection === "Scissors" && computerSelection === "Paper")
@@ -39,9 +39,29 @@ function playRound(playerSelection, computerSelection)
     {
         console.log("Player Wins! Paper beat Rock")
     }
+    // scenario where the computer wins
+    else if(computerSelection === "Rock" && playerSelection === "Scissors")
+    {
+        console.log("Computer Wins! Rock beats Scissors")
+    }
+    // if(Scissors > Paper ) print winner
+    else if(computerSelection === "Scissors" && playerSelection === "Paper")
+    {
+        console.log("Computer Wins! Scissors beat Paper")
+    }
+    // if(Paper > Rock) print winner
+    else if(computerSelection === "Paper" && playerSelection === "Rock")
+    {
+        console.log("Computer Wins! Paper beat Rock")
+    }
+
     else if(playerSelection === "Paper" && computerSelection === "Paper" || playerSelection === "Rock" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Scissors")
     {
         console.log("Its a tie");
+    }
+    else
+    {
+        console.log("Please select either 'Rock', 'Paper' or 'Scissors'")
     }
 }
 
@@ -50,7 +70,8 @@ const computerSelection = getComputerChoice();
 console.log("Computer choice: " + computerSelection)
 
 // 4. Print out choice of player
-const playerSelection = "Rock";
+const playerSelection = prompt("The choice you played is ");
+
 console.log("Player choice: " + playerSelection)
 
 // 5. Print out the winner of the game
