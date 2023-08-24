b003e866b37668fe15ffcf8fa06078650dad9ff4
David <Dzilla398@gmail.com>
2023-08-24 15:44:10 +0930

    refactor: removed console.logs and used DOM manipulation to print out message if player or computer wins

--- javascript.js
+++ javascript.js
@@ -1,25 +1,28 @@
 // select the button
 // use for each method to iterate through each button
 // for each one we add a 'click' listener to the playRound() with the correct playerSelection
-const btn = document.querySelectorAll('.selectButton');
-btn.forEach((button) => {
-   button.addEventListener('click',function(){
-       // if the user selected the
-       let playerChoice = "Rock";
-       if(playerChoice)
-       {
-           playRound(playerChoice, getComputerChoice());
-       }
-       else if(playerChoice === "Paper")
-       {
-           playRound(playerChoice,getComputerChoice());
-       }
-       else if(playerChoice === "Scissors")
-       {
-           playRound(playerChoice, getComputerChoice());
-       }
-   });
-});
+function playGame(){
+    const btn = document.querySelectorAll('.selectButton');
+    btn.forEach((button) => {
+        button.addEventListener('click',function(){
+            // if the user selected the
+            let playerChoice = "Rock";
+            if(playerChoice)
+            {
+                playRound(playerChoice, getComputerChoice());
+            }
+            else if(playerChoice === "Paper")
+            {
+                playRound(playerChoice,getComputerChoice());
+            }
+            else if(playerChoice === "Scissors")
+            {
+                playRound(playerChoice, getComputerChoice());
+            }
+        });
+    });
+}
+
 
 // 1. Create a function called getComputerChoice that handles the computer inputs random choice between Rock, Paper and
 function getComputerChoice()
@@ -43,82 +46,128 @@ let computerScore = 0;
 
 function playRound(playerSelection, computerSelection)
 {
+    const divMessage = document.createElement('div');
+    const btn = document.querySelector(".selectButton");
+
     // Use an if statement to show possible conditions if the player won
     if(playerSelection === "Rock" && computerSelection === "Scissors")
     {
         playerScore++;
+        const msg = document.createTextNode("Player Has Won. P: " + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log( "Player Wins! Rock beats Scissors. Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(playerSelection === "Scissors" && computerSelection === "Paper")
     {
         playerScore++;
+        const msg = document.createTextNode("Player Has Won. P: " + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log("Player Wins! Scissors beat Paper Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(playerSelection === "Paper" && computerSelection === "Rock")
     {
         playerScore++;
+        const msg = document.createTextNode("Player Has Won. P: " + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log("Player Wins! Paper beat Rock Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(computerSelection === "Rock" && playerSelection === "Scissors")
     {
+        const msg = document.createTextNode("Comp Has Won. P:" + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
         computerScore++;
+        // winnerOfGame();
         console.log("Computer Wins! Rock beats Scissors Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(computerSelection === "Scissors" && playerSelection === "Paper")
     {
         computerScore++;
+        const msg = document.createTextNode("Comp Has Won. P:" + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log("Computer Wins! Scissors beat Paper. Score is Player Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(computerSelection === "Paper" && playerSelection === "Rock")
     {
         computerScore++;
+        const msg = document.createTextNode("Comp Has Won. P:" + playerScore + ' C: ' + computerScore);
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log("Computer Wins! Paper beat Rock Current score is Player: " + playerScore + " Computer Score: " + computerScore);
     }
     else if(playerSelection === computerSelection)
     {
         playerScore++;
         computerScore++;
+
+        const msg = document.createTextNode("It's a Tie!. P: " + playerScore + ' C: ' + computerScore );
+        btn.appendChild(divMessage).appendChild(msg);
+        // winnerOfGame();
         console.log("Its a tie. Current Score is Player: " + playerScore + " Computer Score: "+ computerScore);
     }
 }
 
+// function winnerOfGame(){
+//     const divMessage = document.createElement('div');
+//     const btn = document.querySelector(".selectButton");
+//     // if(playerScore > computerScore)
+//     // {
+//     //     const msg = document.createTextNode("Player Has Won. P: " + playerScore + ' C: ' + computerScore);
+//     //     btn.appendChild(divMessage).appendChild(msg);
+//     // }
+//     // else if(playerScore < computerScore)
+//     // {
+//     //     const msg = document.createTextNode("Comp Has Won. P:" + playerScore + ' C: ' + computerScore);
+//     //     btn.appendChild(divMessage).appendChild(msg);
+//     // }
+//     // else
+//     // {
+//     //     const msg = document.createTextNode("It's a Tie!. P: " + playerScore + ' C: ' + computerScore );
+//     //     btn.appendChild(divMessage).appendChild(msg);
+//     // }
+// }
+
+
 // function to create a new winner
-function winnerGame()
-{
-    if(playerScore > computerScore)
-    {
-        console.log("\n The Player has Won the game");
-    }
-    else if(playerScore < computerScore)
-    {
-        console.log("\n The Computer has Won the game");
-    }
-    // else if(playerScore === computerScore)
-    // {
-    //     console.log("Its a Tie!. Thank you for playing");
-    // }
-}
+// function winnerGame()
+// {
+//     if(playerScore > computerScore)
+//     {
+//         // console.log("\n The Player has Won the game");
+//     }
+//     else if(playerScore < computerScore)
+//     {
+//         // console.log("\n The Computer has Won the game");
+//     }
+//     // else if(playerScore === computerScore)
+//     // {
+//     //     console.log("Its a Tie!. Thank you for playing");
+//     // }
+// }
 
 // 5. game function used to play a number of games
-function game()
-{
-    // for(let i = 0; i<5; i++)
-    // {
-    //     // 3. Print out choice of computer
-    //     const computerSelection = getComputerChoice();
-    //     console.log("Computer choice is: " + computerSelection);
-    //
-    //     // 4. Print out choice of player
-    //     const playerSelection = prompt("The choice you played is ");
-    //     console.log("Player choice is: " + playerSelection);
-    //
-    //     playRound(playerSelection, computerSelection);
-    //     console.log("\n");
-    // }
-    // print winner of game
-    winnerGame();
-}
+// function game()
+// {
+//     // for(let i = 0; i<5; i++)
+//     // {
+//     //     // 3. Print out choice of computer
+//     //     const computerSelection = getComputerChoice();
+//     //     console.log("Computer choice is: " + computerSelection);
+//     //
+//     //     // 4. Print out choice of player
+//     //     const playerSelection = prompt("The choice you played is ");
+//     //     console.log("Player choice is: " + playerSelection);
+//     //
+//     //     playRound(playerSelection, computerSelection);
+//     //     console.log("\n");
+//     // }
+//     // print winner of game
+//     winnerGame();
+// }
+//
+// game();
 
-game();
-
-
+playGame();
